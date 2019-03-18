42b8fd14076f6327b63fb5c03e8572d8ad9e30d0
Dave Sarbora and Sam Stoia <Guest@epicodus-32.local>
2019-03-18 16:28:54 -0700

    a

---
+++ src/main.js
@@ -0,0 +1,62 @@
+import './styles.css';
+// import Charmander from './charmander.png';
+//
+//
+//
+// var myCharmander = new Image();
+// myCharmander.src = Charmander;
+//
+// element.appendChild(myCharmander)
+
+$(document).ready(function(){
+
+  $("#1").click(function(){
+    $("#squirtle1").show();
+  });
+
+  $("#2").click(function(){
+    $("#pikachu1").show();
+  });
+
+  $("#3").click(function(){
+    $("#charmander1").show();
+  });
+
+  $("#4").click(function(){
+    $("#weedle1").show();
+  });
+
+  $("#5").click(function(){
+    $("#charmander2").show();
+  });
+
+  $("#6").click(function(){
+    $("#bulbasaur1").show();
+  });
+
+  $("#7").click(function(){
+    $("#squirtle2").show();
+  });
+
+  $("#8").click(function(){
+    $("#snorlax1").show();
+  });
+
+  $("#9").click(function(){
+    $("#snorlax2").show();
+  });
+
+  $("#10").click(function(){
+    $("#bulbasaur2").show();
+  });
+
+  $("#11").click(function(){
+    $("#pikachu2").show();
+  });
+
+  $("#12").click(function(){
+    $("#weedle2").show();
+  });
+
+
+});
